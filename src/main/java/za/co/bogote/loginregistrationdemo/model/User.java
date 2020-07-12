652bb54009c9e0e4d06cc3ea0365c43ca810ac2a
RossK <kelobang.opatile@gmail.com>
2020-07-12 20:39:55 +0200

    User and Role Entity mapping done

--- src/main/java/za/co/bogote/loginregistrationdemo/model/User.java
+++ src/main/java/za/co/bogote/loginregistrationdemo/model/User.java
@@ -1,13 +1,33 @@
 package za.co.bogote.loginregistrationdemo.model;
 
+import javax.persistence.*;
 import java.util.Collection;
 
+@Entity
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+
+
     private Collection<Role> roles;
 
     public User(String firstName, String lastName, String email, String password, Collection<Role> roles) {
