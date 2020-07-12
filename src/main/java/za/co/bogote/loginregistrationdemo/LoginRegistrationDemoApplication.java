1026e9b089a93c50d068caa98dca9d79a2ac55de
RossK <kelobang.opatile@gmail.com>
2020-07-12 18:53:23 +0200

    Initial commit

---
+++ src/main/java/za/co/bogote/loginregistrationdemo/LoginRegistrationDemoApplication.java
@@ -0,0 +1,13 @@
+package za.co.bogote.loginregistrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginRegistrationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/bogote/loginregistrationdemo/LoginRegistrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bogote.loginregistrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
